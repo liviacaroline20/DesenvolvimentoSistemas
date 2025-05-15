b026652cedce927e6091877fbdafaabd807808fc
Livia <liviacaroline423@gmail.com>
2025-05-15 18:29:26 +0000

    exercicio

--- ExercicioAula003/ListaQ16.js
+++ ExercicioAula003/ListaQ16.js
@@ -5,7 +5,6 @@
 let nota1 = 70;
 let nota2 = 40; 
 let nota3 = 70;
-let = media;
 
  if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10) {
     Console.log("Todas as notas devem estar entre 0 e 10. Tente novamente.");
--- ExercicioAula003/ListaQ21.js
+++ ExercicioAula003/ListaQ21.js
@@ -1,2 +1,30 @@
 // 21. Faça um programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2-
 // Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
+let bruto = 1000.00; 
+let ir;
+let inss = salarioBruto * 0.10;
+let sindicato = salarioBruto * 0.03;
+let descontos;
+let liquido;
+
+if(bruto <= 900){
+    ir = salarioBruto;
+}
+
+else if(bruto <= 1500){
+    ir = bruto * 0.5
+}
+
+else if(salarioBruto <= 2500){
+    ir = bruto * 0.1 
+}
+
+else{
+    ir = bruto * 0.2;
+}
+
+descontos = ir + sindicato + inss;
+liquido = bruto - descontos; 
+
+console.log(`Seu salario liquido é R$${liquido}`);
