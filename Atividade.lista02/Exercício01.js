766e1442e6dfe0d0eee06575178f999954b08a01
Livia <liviacaroline423@gmail.com>
2025-05-16 16:35:49 +0000

    Atividade - Lista 02

---
+++ Atividade.lista02/Exercício01.js
@@ -0,0 +1,13 @@
+// 01. Crie uma função que receba dois números e retorne a soma deles. 
+
+let n1 = 4;
+let n2 = 6;
+let = soma; 
+
+fuction soma (n1 + n2){
+    console.log('A soma')
+}
+
+return{
+    (n1 + n2)
+} 
