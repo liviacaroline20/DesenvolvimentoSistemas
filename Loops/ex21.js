2895b5ebd61e752075a98af99a506313309bfc57
Livia <liviacaroline423@gmail.com>
2025-06-03 02:26:05 +0000

    lista

---
+++ Loops/ex21.js
@@ -0,0 +1,9 @@
+// 21. Faça um programa que verifique se 121 é um número primo.
+
+let numero = 121; 
+     if (numero % 2 == 0 && numero % numero == 0) {   
+      console.log("Esse número é primo");   
+ } 
+ else {       
+      console.log("Esse número não é primo");   
+    } 
